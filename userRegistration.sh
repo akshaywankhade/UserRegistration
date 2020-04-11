4e4bc0812a89d067d51b315cc0fbecbbee877204
Akshay Wankhade <akshuwankhade12@gmail.com>
2020-04-11 07:30:34 +0530

    ADDED: Validating Email Id

--- userRegistration.sh
+++ userRegistration.sh
@@ -3,15 +3,29 @@
 #TAKING INPUTS
 read -p "Enter First Name :  " firstName
 read -p "Enter Last Name : " lastName
+read -p "Enter Email Id : " email
 
+#FUNCTION TO VALIDATE NAME
 function checkName(){
 	patternName="^([A-Z][a-z]{1,10})$"
-	if [[ $1 =~ $patternName ]] && [[ $2 =~ $patternName ]]
+	if [[ $firstName =~ $patternName ]] && [[ $lastName =~ $patternName ]]
 	then
 		echo "Valid is First and Last Name"
 	else
-		echo "Invalid is First and Last Name "
+		echo "Invalid is First and Last Name"
+	fi
+}
+
+#FUNCTION TO VALIDATE EMAIL ID
+function checkEmail(){
+	patternEmail="^abc((\.[A-Z]+[a-z]*[0-9]*)|(\.[A-Z]*[a-z]+[0-9]*)|(\.[A-Z]*[a-z]*[0-9]+)|^)?@bl\.co(\.[a-z]+){1,}$"
+	if [[ $email =~ $patternEmai ]]
+	then
+		echo "Valid is Email Id"
+	else
+		echo "Invalid is Email Id"
 	fi
 }
 
 checkName $firstName $lastName
+checkEmail $email
